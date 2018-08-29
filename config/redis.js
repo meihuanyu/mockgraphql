a965356a086a38942ba82184a84fb2cfea8b717b
meiyu <meiyu_2018>
2018-08-29 14:23:38 +0800

    引入redis保存项目结构数据

---
+++ config/redis.js
@@ -0,0 +1,9 @@
+import ioRedis from 'ioredis'
+const redis=new ioRedis({
+    host : '172.96.221.198',//安装好的redis服务器地址
+    port : 6379,　//端口
+    prefix : 'sam:',//存诸前缀
+    ttl : 60 * 60 * 23,//过期时间
+    db: 0
+})
+export default redis
