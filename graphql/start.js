2d794c5fcf4b8fc33e7352b279b8d53ca166d712
meiyu <meiyu_2018>
2018-08-30 17:21:53 +0800

    f废弃

--- graphql/start.js
+++
@@ -1,6 +0,0 @@
-import schema from './graphqlQuery';
-import { querySchme } from '../controllers/structure'
-
-const xx=new schema()
-const _schema=xx.startSchema(querySchme)
-export default _schema
