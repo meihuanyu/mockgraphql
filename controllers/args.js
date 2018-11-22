2424c73071e807b7f224a3795d634c2196009938
meihuanyu <763084024@qq.com>
2018-11-22 13:54:47 +0800

    添加索引参数

--- controllers/args.js
+++ controllers/args.js
@@ -1,5 +1,5 @@
 import db from '../config/database'
-
+import { updateData ,addData} from './sql'
 export const query_args=async (ctx)=>{
     const sql = `select * from system_arg where tableid=? order by type DESC`
     const res = await db.query(sql,[ctx.query.id])
@@ -24,9 +24,12 @@ export const delete_args=async (ctx)=>{
   }
 
 export const update_args=async (ctx)=>{
-    const { id, alias, oper, type } = ctx.query
-    const sql = `update system_arg set alias=? ,oper=? ,type=?   where id=?`
-    const res = await db.query(sql,[alias , oper , type , id])
+    const { name, type ,relationid,id,isupdate,isdelete,iscreate,issingle,islist,isindex} = ctx.query
+    
+    const res = await updateData('system_arg',{
+      name,type,relationid,isupdate,isdelete,iscreate,issingle,islist,isindex
+    },`id=${id}`)
+
     if(res){
       ctx.body={
           success:true,
@@ -36,9 +39,11 @@ export const update_args=async (ctx)=>{
   }
 
 export const create_args=async (ctx)=>{
-    const { alias, oper, type ,tableid} = ctx.query
-    const sql = `insert into  system_arg(alias , oper ,type ,tableid) values (? , ? , ? ,?)`
-    const res = await db.query(sql,[alias , oper , type , tableid])
+    const {  name, type ,relationid,tableid,isupdate,isdelete,iscreate,issingle,islist,isindex} = ctx.query
+    
+    const res = await addData('system_arg',{
+      name, type ,relationid,isupdate,isdelete,iscreate,issingle,islist,isindex,tableid
+    })
     if(res){
       ctx.body={
           success:true,
@@ -47,5 +52,32 @@ export const create_args=async (ctx)=>{
     }
   }
 export const import_args=async (ctx)=>{
-    
+  const { id } = ctx.query
+  const fieldsSql = `select fieldname name,fieldtype type,relationtableid tableid,graprelationid relationid from graphql_field where relationtableid=?`
+  const argsSql = `select name from system_arg where tableid=${id}`
+  const args = await db.query(argsSql)
+  const argsName = args.map(item=>item.name)
+  const fields = await db.query(fieldsSql,[id])
+  let _values=[]
+  for(let i=0;i<fields.length;i++){
+    const {name,type,tableid,relationid} = fields[i]
+    if(argsName.indexOf(name)===-1){
+      _values.push(`('${name}','${type}',${tableid},${relationid})`)
+    }
+  }
+  if(_values.length){
+    ctx.body={
+      success:false,
+      data:[],
+      msg:"没有需要添加的默认参数"
+    }
+  }
+  _values.join(',')
+  const addArgsSql=`insert into system_arg (name,type,tableid,relationid) values ${_values}`
+  console.log(addArgsSql)
+  const res = await db.query(addArgsSql)
+  ctx.body={
+    success:true,
+    data:res
+  }
 }
