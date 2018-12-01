bc4464f3fd223a7ba1a768ca7d99b1985ea284db
meihuanyu <763084024@qq.com>
2018-12-01 17:46:59 +0800

    排序

--- src/initSqlFunction.js
+++ src/initSqlFunction.js
@@ -15,10 +15,37 @@ export const initSql = function(){
         
         WHILE sTempChd IS NOT NULL DO
         SET sTemp= CONCAT(sTemp,',',sTempChd);
-        SELECT GROUP_CONCAT(mid) INTO sTempChd FROM d_menugrant WHERE FIND_IN_SET(pid,sTempChd)>0;
+        SELECT GROUP_CONCAT(id) INTO sTempChd FROM d_menugrant WHERE FIND_IN_SET(pid,sTempChd)>0;
         END WHILE;
         RETURN sTemp;
         END;`)
     })
+    db.query(`DROP FUNCTION IF EXISTS 'nextval';`).then(function(){
+        db.query(`DELIMITER //    
+        CREATE  FUNCTION 'nextval'(seq_name VARCHAR(50)) RETURNS int(11)    
+            DETERMINISTIC     
+        BEGIN     
+        UPDATE sequence SET current_value = current_value + increment WHERE NAME = seq_name;       
+        RETURN currval(seq_name);       
+        END//     
+        DELIMITER ;
+        `)
+    })
+
+    db.query(`DROP FUNCTION IF EXISTS 'currval';`).then(function(){
+        db.query(`DELIMITER //  
+        CREATE  FUNCTION 'currval'(seq_name VARCHAR(50)) RETURNS int(11)  
+            READS SQL DATA  
+            DETERMINISTIC  
+        BEGIN  
+        DECLARE VALUE INTEGER;  
+        SET VALUE = 0;  
+        SELECT current_value INTO VALUE FROM sequence WHERE NAME = seq_name;  
+        RETURN VALUE;  
+        END//  
+        DELIMITER ;
+        `)
+    })
+
     
 }
