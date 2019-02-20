a6ac6c56e3a4e9694a8527ede3919c343aea4621
meihuanyu <763084024@qq.com>
2019-02-20 13:59:13 +0800

    db pass

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 import Client from 'mysql-pro'
 const client = new Client({     
     mysql: {
-        host : '172.96.221.198',
+        host : '47.100.103.106',
         database : 'mock',
         user: 'root',
-        password : '1234'
+        password : 'showrpassword'
     }
 }); 
 
