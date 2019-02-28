6d219998fc18d81f249bdc9226afaf550db73bee
meihuanyu <763084024@qq.com>
2019-02-28 10:08:33 +0800

    redis 配置

--- sh/startKoa.sh
+++ sh/startKoa.sh
@@ -2,6 +2,7 @@
 yarn install
 id=`netstat -nlp | grep :4001 | awk '{print $7}' | awk -F"/" '{ print $1 }'`
 kill -9 $id
-echo 'end prot $id'
+echo "end prot $id"
 yarn install
 npm start &
+echo 'go go go go'
