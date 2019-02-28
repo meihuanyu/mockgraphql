b09d33787a394a6e3f51c2bec5e8dac6a54feb27
meihuanyu <763084024@qq.com>
2019-02-28 16:37:04 +0800

    xxxxxxmmm

--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,4 @@
 import ioRedis from 'ioredis'
-const redis=new ioRedis({
-    host : '118.24.96.18',//安装好的redis服务器地址
-    port : 6379
-})
+const redis=new ioRedis(6379,'118.24.96.18')
 export default redis
 
