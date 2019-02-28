e71ab4bbfb60d8fec80de8d5984cd0819e211b3f
meihuanyu <763084024@qq.com>
2019-02-28 15:43:48 +0800

    xxx

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,6 @@
 import ioRedis from 'ioredis'
 const redis=new ioRedis({
-    host : '127.0.0.1',//安装好的redis服务器地址
+    host : '118.24.96.18',//安装好的redis服务器地址
     port : 6379,　//端口
     prefix : 'sam:',//存诸前缀
     ttl : 60 * 60 * 23,//过期时间
